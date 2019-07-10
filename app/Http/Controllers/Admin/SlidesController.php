53d104224db7f9e324be8212df0b870793449e1c
Saidi Adegoke <rasheedsaidi@gmail.com>
2019-07-10 17:28:11 +0100

    Fix bug on update method

--- app/Http/Controllers/Admin/SlidesController.php
+++ app/Http/Controllers/Admin/SlidesController.php
@@ -125,7 +125,7 @@ class SlidesController extends Controller
         }
         
         $data = $request->all();
-        $data['source'] = ($request->hasFile('slide') && $path)? $imageUpload->id: $slide->cover;
+        $data['source'] = ($request->hasFile('slide') && $path)? $imageUpload->id: $slide->source;
         $slide->update($data);
         if($slide) {
             return redirect()->route('slides.show', ['slide' => $slide->id]);
