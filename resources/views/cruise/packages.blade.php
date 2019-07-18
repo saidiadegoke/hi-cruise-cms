f2471eaaa5fa26883d7f59eda29f777b73a06619
Saidi Adegoke <rasheedsaidi@gmail.com>
2019-07-18 16:49:31 +0100

    pre package sep

--- resources/views/cruise/packages.blade.php
+++ resources/views/cruise/packages.blade.php
@@ -2,23 +2,23 @@
 
 @section("content")
 
-    <section class="set-bg-base">
+<section class="set-bg-base">
       <div class="container">
         <h4 class="all-caps">Packages</h4>
-
-        @foreach ($yachts as $yacht)
-        @php
-          $banner = $banner->where('yacht_id',$yacht->id)->get()->first()->file->filename;    
-        @endphp
+        <!--h4 class="center">Our Yatchs</h4-->
         <div class="col-md-6 flyInRight">
           <div class="container product-box-cont">
             <div class="product-box">
-              <h4>{{$yacht->name}}</h4>
-              {{-- {{ dd($yacht->images)}} --}}
-            <img src="{{asset('public/storage/'.$banner)}}" alt="" />
-              {{-- <img src="{{asset('/assets/img/banners/eugene1_bn.jpg')}}" alt="" /> --}}
+              <h4>Eugene 1</h4>
+              <img src="{{ asset('public/assets/img/banners/eugene1_bn.jpg') }}" alt="" />
               <p class="opaque">
-                {!! $yacht->description !!}
+                Eugene 1 is our luxury 140ft three-storey yacht which comes with
+                top of the range features such as; Exquisite interior design,
+                Fully air-conditioned interior with chilling capacity of
+                528,000BTU, Guest capacity: 600 (Banquet) & 1000 (standing),
+                Automated sunroof, In-built 32 CCTV Cameras, Automated Sensor
+                Doors, Hygienic Toilets, 230KW power generation, 4 Cabin rooms
+                and so much more...
               </p>
               <ul>
                 <li>Exquisite interior design.</li>
@@ -33,19 +33,44 @@
                 <li>Hygienic Toilets</li>
                 <li>4 Cabin rooms and so much more</li>
               </ul>
-              <a href="{{route('package',['yacht'=>$yacht->id])}}" class="btn btn-primary">view packages</a>
+              <a href="{{route('package',['yacht'=> 1])}}" class="btn btn-primary">view packages</a>
             </div>
           </div>
         </div>
-
-        @endforeach
-        
+        <div class="col-md-6 flyInLeft">
+          <div class="container product-box-cont">
+            <div class="product-box">
+              <h4>Eugene</h4>
+              <img src="{{ asset('public/assets/img/banners/eugene_bn.jpg') }}" alt="" />
+              <p class="opaque">
+                Eugene is a private luxurious yacht with a capacity of 8 to 10
+                people.The beautiful leather interior piece sailing at 8 to 10
+                knots cruising speed has a black water tank capacity of
+                220litres and fully air conditioned with 8000 BTU with bathroom
+                shower and kitchenette.This is solely A PRIVATE CRUISE
+              </p>
+              <ul>
+                <li>Exquisite interior design.</li>
+                <li>Exclusive private cruises for adventure</li>
+                <li>Family cruise</li>
+                <li>Couple cruise and lots more</li>
+                <li>Family of up to 10</li>
+                <li>
+                  Honeymoon cruise for 4hrs in private enclosed en-suite cabin
+                </li>
+                <li>Bridal showers for 10</li>
+                <li>Private disco party with Superb Music system</li>
+              </ul>
+              <a href="{{route('package',['yacht'=> 2])}}" class="btn btn-primary">view packages</a>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
    
     <section class="pattern1 no-margin pad-10 mid-space">
       <div class="container">
-        <img src="img/logo-icon.png" alt="" class="logo-icon-section" />
+        <img src="{{ asset('public/assets/img/logo-icon.png') }}" alt="" class="logo-icon-section" />
         <h3 class="all-caps">Dream, Discover, Explore</h3>
       </div>
     </section>
